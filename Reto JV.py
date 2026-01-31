458200d50af532f48e2fd8febcc66783fd3fa124
Puddin <jaimeval03@gmail.com>
2026-01-31 13:26:14 -0600

    Correcion reto viz de datos de python

--- Reto JV.py
+++ Reto JV.py
@@ -27,52 +27,31 @@ print(mexicodatadf)
 
 #histograma
 sb.histplot(data = dfhappy, x='Score',bins=10).set_title('Frequencia de indice de felizidad')
-plt.text(x=6.595, y=12.5, s='Mexico', color='red', ha='center')
+plot_bbox={'boxstyle':'circle', 'facecolor':'green','alpha':0.3}
+plt.text(6.595,12.5,'Mexico\n6.595', bbox=plot_bbox)
 
 #boxplot
-sb.catplot(kind='box', x ='Healthy life expectancy', data=dfhappy)
-plt.text(x=0.861, y=-0.1,s='Mexico', color='red', ha='center')
+sb.catplot(kind='box', x ='Healthy life expectancy', data=dfhappy, color='green')
+
+plt.annotate('Mexico',xy=(0.861,0),xytext=(1,0.1),arrowprops=dict(arrowstyle='->'))
 
 #grafica circular
 dfmexico=mexicodatadf.drop(['Overall rank', 'Score'], axis=1)
 dfmexico = dfmexico.transpose()
 print(dfmexico.head())
-dfmexico.plot.pie(y='Mexico',figsize=(5,5),legend=False).set_title('Factores de felizidad de Mexico')
+dfmexico.plot.pie(y='Mexico',figsize=(5,5), autopct='%1.1f%%',legend=False,color=['red','blue','yellow','green','orange','purple'])
+plt.title('Factores de felizidad de Mexico')
+plt.show()
 
-#Grafica circular de 5 paises
+#Grafica de barras de 5 paises
 dfpaises = dfhappy.loc[(dfhappy['Country or region']=='Finland') | (dfhappy['Country or region']=='Mexico') | (dfhappy['Country or region']== 'South Sudan') | (dfhappy['Country or region']== 'United States') | (dfhappy['Country or region']=='Japan') ]
-dfpaises = dfpaises.set_index('Country or region')
 dfpaises = dfpaises.drop(['Overall rank', 'Score'], axis=1)
-dfpaises = dfpaises.transpose()
+dfpaises = dfpaises.set_index('Country or region')
+dfpaises =dfpaises.transpose().reset_index()
 print(dfpaises)
+dfpaises.plot('index', label='Indice de Felicidad', kind='bar')
 
-#Subplots
-fig=plt.figure(figsize=(20,20))
-fig.suptitle('Evaluacion de felicidad de varios paises', fontsize=18)
-#Finland
-ax1=fig.add_subplot(231)
-dfpaises.plot.pie(y='Finland',figsize=(5,5),ax=ax1,legend=False)
-ax1.set_title('Finland')
 
-#Mexico
-ax2=fig.add_subplot(232)
-dfpaises.plot.pie(y='Mexico',figsize=(5,5),ax=ax2,legend=False)
-ax2.set_title('Mexico')
-
-#South Sudan
-ax3=fig.add_subplot(233)
-dfpaises.plot.pie(y='South Sudan',figsize=(5,5),ax=ax3,legend=False)
-ax3.set_title('South Sudan')
-
-#United States
-ax4=fig.add_subplot(234)
-dfpaises.plot.pie(y='United States',figsize=(5,5),ax=ax4,legend=False)
-ax4.set_title('United States')
-
-#Japan
-ax5=fig.add_subplot(236)
-dfpaises.plot.pie(y='Japan',figsize=(5,5),ax=ax5,legend=False)
-ax5.set_title('Japan')
 
 plt.show()
 
@@ -108,18 +87,30 @@ ax6.set_title('Perceptions of corruption')
 plt.show()
 
 #Heat Map
-heatmapdf = dfhappy.drop('Overall rank',axis=1)
-heatmapdf = heatmapdf.set_index('Country or region')
+heatmapdf = dfhappy.set_index('Country or region')
+corrdf=heatmapdf.corr()
+print(corrdf)
 sb.heatmap(round(heatmapdf.corr(),2),annot=True).set_title('Heatmap de relacion de parametros')
 plt.show()
 
 
 #Join de las 2 tablas para filtrar por region
 dfregions = dfhappy.set_index('Country or region').join(dfmeta.set_index('TableName'), how='left', on='Country or region')
+print(dfregions)
+
+#Obtencion de valores promedio
+dfregions = dfregions.drop(['Country Code','IncomeGroup', 'SpecialNotes','Overall rank'],axis=1).reset_index(drop=True) #drop de valores de texto
+print(dfregions)
+RegionHappy = dfregions.groupby(['Region']).mean()
+RegionHappy = RegionHappy.reset_index()
+print(RegionHappy)
+
+''' Depcrecated average method
 LATAM = dfregions.loc[dfregions['Region']=='Latin America & Caribbean']
 LATAMav = LATAM.loc[:,'Score'].mean()
 
 #Obtencion de average de cada region
+
 Europe = dfregions.loc[dfregions['Region']=='Europe & Central Asia']
 Europeav= Europe.loc[:,'Score'].mean()
 
@@ -138,10 +129,12 @@ SAsiaav = SAsia.loc[:,'Score'].mean()
 Sahara = dfregions.loc[dfregions['Region']=='Sub-Saharan Africa']
 Saharaav = Sahara.loc[:,'Score'].mean()
 
+
+
 RegionHappy = pd.DataFrame({'Region':['Latin America & Caribbean','Europe & Central Asia','East Asia & Pacific','Middle East & North Africa','North America','South Asia','Sub-Saharan Africa'],'Average Happyness':[LATAMav,Europeav,EAsiaav,MidEav,NAav,SAsiaav,Saharaav]})
 print(RegionHappy)
-
-RegionHappy.plot(x='Region',y='Average Happyness', kind='bar', title='Felicidad Promedio por Region', rot=45)
+'''
+RegionHappy.plot(x='Region', y='Score', kind='bar', title='Felicidad Promedio por Region', rot=45,color=['red','blue','yellow','green','orange','purple','Black'])
 plt.show()
 
 
